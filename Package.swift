5fd666b2198af9a8ef2d0212c8e3deb9de2c9602
nhatpv <nhatpv@vietmap.vn>
2023-10-26 11:04:11 +0700

    Change target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [
         .library(
             name: "VietMap",
-            targets: ["VietMap"])
+            targets: ["Mapbox"])
     ],
     dependencies: [
     ],    
